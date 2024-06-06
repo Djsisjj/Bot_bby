fff6d62ffd32ee6b2bdb830ac8ad5fdf05138172
Djsisjj <114715211+Djsisjj@users.noreply.github.com>
2024-06-06 18:53:34 +0600

    Update bangla.js

--- scripts/commands/bangla.js
+++ scripts/commands/bangla.js
@@ -3,7 +3,7 @@ module.exports.config = {
 	version: "1.0.1",
 	permssion: 0,
 	credits: "EMon-BHai",
-  prefix: 'awto',
+  prefix: 'true',
 	description: "Text translation",
 	category: "user",
 	usages: "bangla [Text]",
@@ -34,6 +34,6 @@ module.exports.run = async ({ api, event, args }) => {
 		var text = '';
 		retrieve[0].forEach(item => (item[0]) ? text += item[0] : '');
 		var fromLang = (retrieve[2] === retrieve[8][0][0]) ? retrieve[2] : retrieve[8][0][0]
-		api.sendMessage(`বাংলা-কনভাট:\n\n\n ${text}\n\n──\n𝘽𝘼𝘿𝘽𝙊𝙔`, event.threadID, event.messageID);
+		api.sendMessage(`𝘽𝘼𝙉𝙂𝙇𝘼:\n\n\n ${text}\n\n──\n𝘿𝙊𝙉𝙀`, event.threadID, event.messageID);
 	});
                                     }

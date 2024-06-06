9ff71d3c76de465aa0fd6bbdae689369757928a3
Djsisjj <114715211+Djsisjj@users.noreply.github.com>
2024-06-06 20:22:02 +0600

    Update hot.js

--- scripts/commands/hot.js
+++ scripts/commands/hot.js
@@ -7,7 +7,7 @@ module.exports.config = {
   credits: "Nayan",
   description: "Random hot video",
   prefix: true,
-  category: "Media", 
+  category: "18+", 
   usages: "", 
   cooldowns: 5,
   dependencies: {

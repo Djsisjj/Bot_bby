87c740983f4b18bad5bc1cad8f18e681c9876d70
Djsisjj <114715211+Djsisjj@users.noreply.github.com>
2024-06-10 22:06:43 +0600

    Update tid.js

--- scripts/commands/tid.js
+++ scripts/commands/tid.js
@@ -1,10 +1,11 @@
 module.exports.config = {
 	name: "tid",	
   version: "1.0.0", 
-	hasPermssion: 0,
+	permssion: 0,
 	credits: "MrTomXxX",
 	description: "Get box id", 
-	commandCategory: "group",
+	prefix:"true",
+	category: "group",
 	usages: "tid",
 	cooldowns: 5, 
 	dependencies: '',
@@ -12,4 +13,4 @@ module.exports.config = {
 
 module.exports.run = async function({ api, event }) {
   api.sendMessage("ID of this thread: "+event.threadID, event.threadID, event.messageID);
-};
+},
